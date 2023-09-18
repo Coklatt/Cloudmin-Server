dc1582502ddcd06cd55657abd1f71f860d8fa894
reader00 <qoryamanahputra0708@gmail.com>
2023-09-18 08:03:03 +0700

    original

--- routes/general.js
+++ routes/general.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { getUser, getDashboardStats, getMemory } from '../controllers/general.js';
+import { getUser, getDashboardStats } from '../controllers/general.js';
 
 const router = express.Router();
 
 router.get('/user/:id', getUser);
 router.get('/dashboard', getDashboardStats);
-router.get('/showCPU', getMemory);
 
 export default router;
