aa99f688c677eb6eb20191a001b9ec4f548dbbb1
reader00 <qoryamanahputra0708@gmail.com>
2023-12-05 21:43:40 +0700

    test multi deploy 8

--- controllers/client.js
+++ controllers/client.js
@@ -1,97 +1,11 @@
-import Product from '../models/Product.js';
-import ProductStat from '../models/ProductStat.js';
-import Transaction from '../models/Transaction.js';
-import User from '../models/User.js';
-import getCountryIso3 from 'country-iso-2-to-3';
+import express from 'express';
+import { getProducts, getCustomers, getTransactions, getGeography } from '../controllers/client.js';
 
-export const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        const productsWithStats = await Promise.all(
-            products.map(async (product) => {
-                const stat = await ProductStat.findOne({ productId: product._id });
-                return {
-                    ...product._doc,
-                    stat,
-                };
-            })
-        );
+const router = express.Router();
 
-        res.status(200).json(productsWithStats);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-};
+router.get('/products', getProducts);
+router.get('/customers', getCustomers);
+router.get('/transactions', getTransactions);
+router.get('/geography', getGeography);
 
-export const getAllCustomers = async (req, res) => {
-    try {
-        const customers = await User.find({ role: 'user' }).select('-password');
-        res.status(200).json(customers);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-};
-
-export const getAllTransactions = async (req, res) => {
-    try {
-        // sort should look like this:
-        // {"field": "userId", "sort": "desc"}
-        const { page = 1, pageSize = 20, sort = null, search = '' } = req.query;
-
-        // formatted sort should look like
-        // { userId: -1 }
-        const generateSort = () => {
-            const sortParsed = JSON.parse(sort);
-            const sortFormatted = {
-                [sortParsed.field]: sortParsed.sort === 'asc' ? 1 : -1,
-            };
-            return sortFormatted;
-        };
-
-        const sortFormatted = Boolean(sort) ? generateSort() : {};
-
-        const transactions = await Transaction.find({
-            $or: [
-                { cost: { $regex: new RegExp(search, 'i') } },
-                { userId: { $regex: new RegExp(search, 'i') } },
-            ],
-        })
-            .sort(sortFormatted)
-            .skip(page * pageSize)
-            .limit(pageSize);
-
-        const total = await Transaction.estimatedDocumentCount({
-            name: { $regex: search, $options: 'i' },
-        });
-
-        res.status(200).json({
-            transactions,
-            total,
-        });
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-};
-
-export const getGeography = async (req, res) => {
-    try {
-        const users = await User.find();
-
-        const mappedLocations = users.reduce((acc, { country }) => {
-            const countryISO3 = getCountryIso3(country);
-            if (!acc[countryISO3]) {
-                acc[countryISO3] = 1;
-            }
-            acc[countryISO3]++;
-            return acc;
-        }, {});
-
-        const formattedLocation = Object.entries(mappedLocations).map(([country, count]) => {
-            return { id: country, value: count };
-        });
-
-        res.status(200).json(formattedLocation);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-};
+export default router;
