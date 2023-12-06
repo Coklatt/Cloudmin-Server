90144241ad2e6cb91cfc59e50f7481f48fcb1efd
reader00 <qoryamanahputra0708@gmail.com>
2023-12-06 07:24:54 +0700

    test multi deploy all

--- routes/client.js
+++ routes/client.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { getProducts, getCustomers, getTransactions, getGeography } from '../controllers/client.js';
+import {
+    getAllProducts,
+    getAllCustomers,
+    getAllTransactions,
+    getAllGeography,
+} from '../controllers/client.js';
 
 const router = express.Router();
 
-router.get('/products', getProducts);
-router.get('/customers', getCustomers);
-router.get('/transactions', getTransactions);
+router.get('/products', getAllProducts);
+router.get('/customers', getAllCustomers);
+router.get('/transactions', getAllTransactions);
 router.get('/geography', getGeography);
 
 export default router;
