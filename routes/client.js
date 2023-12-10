18e19995163ee2f1de52685803dd4e924ded425f
reader00 <qoryamanahputra0708@gmail.com>
2023-12-11 03:30:16 +0700

    push original

--- routes/client.js
+++ routes/client.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import {
-    getAllProducts,
-    getAllCustomers,
-    getAllTransactions,
-    getGeography,
-} from '../controllers/client.js';
+import { getProducts, getCustomers, getTransactions, getGeography } from '../controllers/client.js';
 
 const router = express.Router();
 
-router.get('/products', getAllProducts);
-router.get('/customers', getAllCustomers);
-router.get('/transactions', getAllTransactions);
+router.get('/products', getProducts);
+router.get('/customers', getCustomers);
+router.get('/transactions', getTransactions);
 router.get('/geography', getGeography);
 
 export default router;
